b2f7eb1a0d8627292230ea95aca2a148c108547f
Aotsukii <elie@gazel.net>
2019-11-23 11:49:16 +0100

    First version of register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterFormComponent
+
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent implements OnInit {
+export class RegisterFormComponent {
+  RegisterForm = new FormGroup(
+    {
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      mail: new FormControl(''),
+      password: new FormControl(''),
+      phone: new FormControl('')
+    });
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.RegisterForm.value);
   }
-
 }
